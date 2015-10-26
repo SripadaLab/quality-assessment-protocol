255ed6ea17919074ebc80673e2f65e530f472258
Oscar Esteban <code@oscaresteban.es>
2015-10-26 10:11:41 +0100

    new dmri script. fix headers

--- scripts/qap_dmri_spatial.py
+++ scripts/qap_dmri_spatial.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
+# @Author: Oscar Esteban
+# @Date:   2015-10-26 10:10:28
+# @Last Modified by:   Oscar Esteban
+# @Last Modified time: 2015-10-26 10:10:42
 
 from qap.cli import QAProtocolCLI
 
