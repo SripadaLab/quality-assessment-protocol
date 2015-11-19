5e29abd4e6b08978f22243583c1d35cf9141383e
Oscar Esteban <code@oscaresteban.es>
2015-11-19 16:19:41 +0100

    fix pep8 in setup

--- setup.py
+++ setup.py
@@ -5,27 +5,28 @@ def main():
     from setuptools import setup
 
     exec(open('qap/version.py').read())
-    setup(name='qap',
-          version=__version__,
-          description='A collection of three quality assessment pipelines ' \
-                      'for anatomical MRI and functional MRI scans.',
-          author='Cameron Craddock, Zarrar Shehzad, Steven Giavasis,' \
-                 'Daniel Clark',
-          author_email='cameron.craddock@childmind.org',
-          url='https://github.com/preprocessed-connectomes-project/' \
-              'quality-assessment-protocol',
-          download_url='https://github.com/preprocessed-connectomes-project/'\
-                       'quality-assessment-protocol/tarball/1.0.2',
-          license='',
-          packages=['qap', 'qap.viz', 'qap.workflows'],
-          package_data={'qap': ['inpoint*.txt',
-                                'test_data/*.nii.gz',
-                                'test_data/workflow_reference/*/*',
-                                'test_data/*/*/*/*/*']},
-          scripts=glob("scripts/*"),
-          install_requires=["scipy", "nipype", "nibabel", "nitime", "pyyaml",
-                            "pandas", "seaborn", "pyPdf2", "xhtml2pdf"],
-          zip_safe=False)
+    setup(
+        name='qap',
+        version=__version__,
+        description='A collection of three quality assessment pipelines '
+                    'for anatomical MRI and functional MRI scans.',
+        author='Cameron Craddock, Zarrar Shehzad, Steven Giavasis,'
+               'Daniel Clark',
+        author_email='cameron.craddock@childmind.org',
+        url='https://github.com/preprocessed-connectomes-project/'
+            'quality-assessment-protocol',
+        download_url='https://github.com/preprocessed-connectomes-project/'
+                     'quality-assessment-protocol/tarball/1.0.2',
+        license='',
+        packages=['qap', 'qap.viz', 'qap.workflows'],
+        package_data={'qap': ['inpoint*.txt',
+                              'test_data/*.nii.gz',
+                              'test_data/workflow_reference/*/*',
+                              'test_data/*/*/*/*/*']},
+        scripts=glob("scripts/*"),
+        install_requires=["scipy", "nipype", "nibabel", "nitime", "pyyaml",
+                          "pandas", "seaborn", "pyPdf2", "xhtml2pdf"],
+        zip_safe=False)
 
 if __name__ == "__main__":
 
